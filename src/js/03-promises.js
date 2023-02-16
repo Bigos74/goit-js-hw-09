bfef53e1bdabbca6b3644dff49e630ef1d35bdf0
Oleksandr Revva <revvao@gmail.com>
2023-02-16 17:52:00 +0200

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  // let delay = e.currentTarget.delay.value;
+  // let step = e.currentTarget.step.value;
+  // let amount = e.currentTarget.amount.value;
+  let { delay, step, amount } = Object.fromEntries(new FormData(e.target));
+  delay = +delay;
+  step = +step;
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
