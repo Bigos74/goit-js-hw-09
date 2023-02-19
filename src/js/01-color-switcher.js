3a098aedeb2595a4d5dd9fda0d2b32562cd0d1ed
Oleksandr Revva <revvao@gmail.com>
2023-02-19 19:58:05 +0200

    remake task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -23,54 +23,21 @@ function addColor() {
 
 let intervalId;
 
+function handleStatus(status) {
+  const nextStatus = status === 'start' ? 'stop' : 'start';
+  BUTTONS_UI.activeBtn = nextStatus;
+  btn.classList.add(BUTTONS_UI[nextStatus].class);
+  btn.classList.remove(BUTTONS_UI[status].class);
+  btn.textContent = BUTTONS_UI[nextStatus].text;
+}
+
 function handleButtonBehavior() {
   const { activeBtn } = BUTTONS_UI;
   if (activeBtn === 'stop') {
     clearInterval(intervalId);
-    BUTTONS_UI.activeBtn = 'start';
-    btn.classList.add(true);
-    btn.classList.remove(false);
-    btn.textContent = BUTTONS_UI.start.text;
   } else if (activeBtn === 'start') {
     intervalId = setInterval(addColor, 1000);
-    BUTTONS_UI.activeBtn = 'stop';
-    btn.classList.remove(BUTTONS_UI.start.class);
-    btn.classList.add(BUTTONS_UI.stop.class);
-    btn.textContent = BUTTONS_UI.stop.text;
   }
+  handleStatus(activeBtn);
 }
 handleButtonBehavior();
-
-// const startBtn = document.querySelector('[data-start]');
-// const stopBtn = document.querySelector('[data-stop]');
-// const body = document.body;
-
-// startBtn.addEventListener('click', startColorChange);
-// stopBtn.addEventListener('click', stopColorChange);
-
-// stopBtn.setAttribute('disabled', true);
-// let timerId = null;
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// function timerForColorChange() {
-//   timerId = setInterval(() => {
-//     body.style.backgroundColor = getRandomHexColor();
-//   }, 1000);
-// }
-
-// function startColorChange() {
-//   timerForColorChange();
-
-//   stopBtn.removeAttribute('disabled');
-//   startBtn.setAttribute('disabled', true);
-// }
-
-// function stopColorChange() {
-//   clearInterval(timerId);
-
-//   stopBtn.setAttribute('disabled', true);
-//   startBtn.removeAttribute('disabled');
-// }
